d5596ac6db263d39e78d4cfc88b69f4a35478dda
Lance Easley <leasley21@basecampcodingacademy.org>
2020-11-19 14:29:24 -0600

    Working on world gen

--- classes.py
+++ classes.py
@@ -1,15 +1,58 @@
 import pygame
-
-class Player:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+from pprint import pprint
 
 
-class Block:
-    def __init__(self, x, y, id):
-        self.x = x
-        self.y = y
-        self.id = id
+class Player(object):
+    def __init__(self, x, y, color, map_obj):
+        self.width = 52
+        self.x = x - self.width // 2
+        self.height = 82
+        self.y = y - self.height // 2
+        self.color = color
+        self.hitbox = pygame.Rect(self.x - 2, self.y - 2, self.width + 2, self.height + 2)
+        self.x_coord = map_obj.x
+        self.y_coord = map_obj.y
 
+    def draw(self, window):
+        pygame.draw.rect(window, self.color, (self.x + 1, self.y + 1, self.width - 2, self.height - 2))
     
+    def convert_to_data(self):
+        return self.x_coord, self.y_coord, self.color
+
+
+class Map(object):
+    def __init__(self, world):
+        self.x = 0
+        self.y = 0
+        self.width = 1600
+        self.height = 1600
+        self.x_vel = 5
+        self.y_vel = 5
+        self.world_data = []
+        with open(f"{world}.txt") as world_f:
+            for layer in world_f.readlines():
+                layer_list = []
+                for tile in layer:
+                    if tile != "\n":
+                        layer_list.append(tile)
+                self.world_data.append(layer_list)
+            
+
+    def wall_objs(self):
+        tile_rects = []
+        y = 0
+        for layer in self.world_data:
+            x = 0
+            for tile in layer:
+                if tile != '0':
+                    tile_rects.append(pygame.Rect(x*16,y*16,16,16))
+                x += 1
+            y += 1
+        return tile_rects
+    
+    def draw_collision(self, window):
+        for wall in self.wall_objs():
+            pygame.draw.rect(window, (0,255,0), wall, 1)
+
+if __name__ == "__main__":
+    Map("world")
--- main.py
+++ main.py
@@ -1,24 +1,82 @@
 import pygame
+from classes import Player, Map
 from pprint import pprint
-
-with open("world.txt") as world_f:
-    world_data = world_f.readlines()
-    for index, line in enumerate(world_data):
-        world_data[index] = line.strip('\n')
-    pprint(world_data)
 
 pygame.init()
 
-screen_x, screen_y = (800,800)
+screen_x, screen_y = (1000,800)
 display = pygame.display.set_mode((screen_x, screen_y))
 
 
+def collide_check(rect, colls):
+    collisions = []
+    for wall in colls:
+        if rect.colliderect(wall):
+            collisions.append(world.wall_objs().index(wall))
+    return collisions
 
+
+def redrawGameWindow():
+    world.draw_collision(display)
+    p1.draw(display)
+    pygame.display.update()
+
+
+#mainloop
+world = Map("world")
+clock = pygame.time.Clock()
+collision_tolerance = max(world.x_vel, world.y_vel) * 2 + 1
+p1 = Player(screen_x // 2, screen_y // 2, (255,0,0), world)
 run = True
 while run:
-    display.fill((0,0,0))
+    clock.tick(60)
+
+    display.fill((125,200,255))
+
+    w_coll = True
+    a_coll = True
+    s_coll = True
+    d_coll = True
+
+    redrawGameWindow()
+    # skeld.wall_objs()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    keys = pygame.key.get_pressed()
     
+    coll_index = collide_check(p1.hitbox, world.wall_objs())
+    if coll_index != []:
+        for i in coll_index:
+            if abs(world.wall_objs()[i].bottom - p1.hitbox.top) < collision_tolerance:
+                w_coll = False
+            if abs(world.wall_objs()[i].right - p1.hitbox.left) < collision_tolerance:
+                a_coll = False
+            if abs(world.wall_objs()[i].top - p1.hitbox.bottom) < collision_tolerance:
+                s_coll = False
+            if abs(world.wall_objs()[i].left - p1.hitbox.right) < collision_tolerance:
+                d_coll = False
+
+    if keys[pygame.K_w]:
+        if w_coll:
+            world.y += world.y_vel
+    if keys[pygame.K_a]:
+        if a_coll:
+            world.x += world.x_vel
+    if keys[pygame.K_s]:
+        if s_coll:
+            world.y -= world.y_vel
+    if keys[pygame.K_d]:
+        if d_coll:
+            world.x -= world.x_vel
+    if keys[pygame.K_c]:
+        print(world.x, world.y)
+    
+
+    if keys[pygame.K_ESCAPE]:
+        display = pygame.display.set_mode((screen_x, screen_y))
+        run = False
+
+pygame.quit()
